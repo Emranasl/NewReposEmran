39d59a1c19a543b0b796d7ce2a7f07a9d49dd2b1
Emranasl <emrannn.aslll@gmail.com>
2023-01-28 05:11:23 -0800

    add class Hello

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
 
 
-x = 'hello mr fotoohi'
-print(x)
+# x = 'hello mr fotoohi'
+# print(x)
+class Hello:
+    def __init__(self, name, lastname):
+        self.name = name
+        self.lastname = lastname
+    
+        
+p1 = Hello("mohammad", "fotoohi")
+p2 = Hello( "emran", "shayesteh")
+
+print(f'Hello {p1.name} {p1.lastname}')
+print(f'Hello{p2.name} {p2.lastname}')
 
